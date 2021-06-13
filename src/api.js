03b3e823e372585518ea88929f9d8846dbb6cd88
Dario2002 <banfi.dario@iisgalvanimi.edu.it>
2021-06-13 09:37:55 +0000

    andra?3

--- src/api.js
+++ src/api.js
@@ -2,11 +2,27 @@ const express = require('express');
 const serverless = require ('serverless-http');
 var indexRouter = require('./routes/index');
 
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+
+
+
 const app = express();
 const router = express.Router();
 
  //E' necessario usare per tutte le route il prefisso /.netlify/functions
  app.use('/.netlify/functions/api/index', indexRouter);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(new cors());
+
 
  //NB!
  //NON è necessario avviare il server perchè stiamo creando una lambda function
